e315e82a9b59962a3573c0c46ecdfc19f7a21198
Tomide Adeyanju <tadeyanju@infinion.co>
2026-01-09 15:30:56 +0100

    added env

--- LSBC-VM/LSBC_Backend/LSBC_Backend/settings.py
+++ LSBC-VM/LSBC_Backend/LSBC_Backend/settings.py
@@ -11,16 +11,19 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cik*4-t35gva1=x*a$3m_()x!ov@c2_%i8zzlz73=l$kn+dbj_'
+SECRET_KEY = os.getenv('Secret_Key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -128,5 +131,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MONGO_URI = "mongodb+srv://adeyanjutomide:Adeyanju5241@cluster0.vwh2wm9.mongodb.net/?appName=Cluster0"
+MONGO_URI = os.getenv('URI')
 MONGO_DB_NAME = 'LSBC_DB'
